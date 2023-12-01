aed91feeb26c197a470894081576964307ac892b
denao <denisbatera42@gmail.com>
2023-12-01 10:29:48 -0300

    nova function do cadastro

--- cadastro.js
+++ cadastro.js
@@ -2,4 +2,8 @@ function signUp(email){
     return email;
   
 }
+function signUp(casa){
+    return casa;
+}
+// novo teste de funcionalidade do cadastro 
 //pegando o email e cadastrando na plataforma
